88805c90bbe47c14204b4a8d8357bce7e5ce5dcf
juanjoseholguin <juanjoseholguin128@gmail.com>
2026-04-14 10:41:57 -0500

    cambiar img de pelis

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ export const Home = () => {
   return (
     <div>
       <h1>pagina de inicio</h1>
+      <p>ingresa a catalogo para ver las mejores peliculas y guardar tus favoritas</p>
       <nav className="nav-links">
         <Link to="/catalogue">catalogo</Link>
         <Link to="/saved">guardadass</Link>
