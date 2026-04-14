bff5707b51072da438a48369a320c48f77d07b4c
juanjoseholguin <juanjoseholguin128@gmail.com>
2026-04-14 10:44:52 -0500

    commit final

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 export const Home = () => {
   return (
     <div>
-      <h1>pagina de inicio</h1>
-      <p>ingresa a catalogo para ver las mejores peliculas y guardar tus favoritas</p>
+      <h1>pagina de inicio holguin :D</h1>
+      <p>
+        ingresa a catalogo para ver las mejores peliculas y guardar tus
+        favoritas
+      </p>
       <nav className="nav-links">
         <Link to="/catalogue">catalogo</Link>
         <Link to="/saved">guardadass</Link>
